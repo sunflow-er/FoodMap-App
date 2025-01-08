3da24dd2e44067374f52252e9a5d496b7717313b
sunflow-er <antjd03170@gmail.com>
2025-01-09 00:24:04 +0900

    지도에 맛집 목록 표시하기

--- app/src/main/java/com/masonk/foodmap/MainActivity.kt
+++ app/src/main/java/com/masonk/foodmap/MainActivity.kt
@@ -2,16 +2,20 @@ package com.masonk.foodmap
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.masonk.foodmap.databinding.ActivityMainBinding
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.Tm128
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -38,23 +42,81 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         // 비동기로 NaverMap 객체 얻기 -> 콜백
         binding.mapView.getMapAsync(this)
 
-        SearchRepository.getFamousRestaurantList("대구").enqueue(object : Callback<FamousRestaurantList> {
-            override fun onResponse(
-                p0: Call<FamousRestaurantList>,
-                response: Response<FamousRestaurantList>
-            ) {
-                if (response.isSuccessful) {
-                    Log.e("SUCCESS", "${response.body().toString()}")
+        // 검색뷰 리스너 설정
+        binding.searchView.setOnQueryTextListener(object : OnQueryTextListener {
+            // Called when the user submits the query
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // 검색어가 있으면
+                if (query?.isNotEmpty() == true) {
+                    // 검색
+                    SearchRepository.getFamousRestaurantList(query).enqueue(object : Callback<FamousRestaurantList> {
+                        override fun onResponse(
+                            p0: Call<FamousRestaurantList>,
+                            response: Response<FamousRestaurantList>
+                        ) {
+                            // 검색 결과 가져오기
+                            val famousRestaurantList = response.body()?.items.orEmpty()
+
+                            Log.e("famousRestaurantList",famousRestaurantList.toString())
+
+                            // 검색 결과가 없을 때
+                            if (famousRestaurantList.isEmpty()) {
+                                Toast.makeText(this@MainActivity, "검색 결과가 없습니다.", Toast.LENGTH_SHORT).show()
+                                return
+                            } else if (!isMapInit) { // 지도가 초기화되지 않았을 때
+                                Toast.makeText(this@MainActivity, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                                return
+                            }
+
+                            // 검색 결과를 바탕으로 마커 리스트 만들기
+                            val markerList = famousRestaurantList.map {
+                                // 마커 객체 생성
+                                Marker().apply {
+                                    // 위치, 좌표
+                                    position = LatLng((it.mapy / 10_000_000.0), (it.mapx / 10_000_000.0)) // WGS84 -> WGS84
+
+                                    // 캡션
+                                    captionText = it.title
+                                    
+                                    // 마커와 네이버맵 연결
+                                    map = naverMap
+                                }
+                            }
+
+                            // 지도를 바라보는 카메라의 이동을 정의한 객체
+                            val cameraUpdate = CameraUpdate
+                                .scrollTo(markerList.first().position) // 첫 번째 마커의 위치로 카메라 이동
+                                .animate(CameraAnimation.Easing) // 카메라 이동 시 애니메이션 적용, 부드럽게 가감속되는 애니메이션
+
+                            Log.e("POSITION", markerList.first().position.toString())
+
+                            // 카메라 이동
+                            naverMap.moveCamera(cameraUpdate)
+                        }
+
+                        override fun onFailure(p0: Call<FamousRestaurantList>, t: Throwable) {
+                            Log.e("FAILURE", "${t.message}")
+                        }
+
+                    })
+
+                    // 기본 동작 수행
+                    // 키보드 숨기기, 검색어 유지
+                    return false
                 } else {
-                    Log.e("ERROR", "Response code: ${response.code()}, message: ${response.message()}")
+                    // 기본 동작을 막음
+                    return true
                 }
             }
 
-            override fun onFailure(p0: Call<FamousRestaurantList>, t: Throwable) {
-                Log.e("FAILURE", "${t.message}")
+            // Called when the query text is changed
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
             }
 
         })
+
+
     }
 
     override fun onStart() {
@@ -96,10 +158,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(map: NaverMap) {
         naverMap = map
         isMapInit = true
-
-        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.3666102, 126.9783881))
-            .animate(CameraAnimation.Easing)
-        naverMap.moveCamera(cameraUpdate)
     }
 
 }
